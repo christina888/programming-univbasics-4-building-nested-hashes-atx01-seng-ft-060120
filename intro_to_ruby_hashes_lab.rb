b3572364bab8e5f8061a446a28aa16f4a0f19d5e
Christina Chianelli <cechianelli@gmail.com>
2020-05-23 19:51:01 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,9 +1,6 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-  new_hash = {}
-  new_hash[:railroads] = {}
-  new_hash
 
 end
 
