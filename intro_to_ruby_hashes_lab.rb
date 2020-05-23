aaa0310c11d1e80e36225862af6459764278e021
Christina Chianelli <cechianelli@gmail.com>
2020-05-23 20:15:12 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,9 +32,9 @@ def monopoly_with_third_tier
   new_hash[:railroads][:rent_in_dollars][:one_piece_owned] = "25"
   
   new_hash[:railroads][:names][:reading_railroad] = {}
-  new_hash[:railroads[:pennsylvania_railroad] = {}
-  new_hash[:railroads][:b_and_o_railroad] = {}
-  new_hash[:railroads][:shortline_railroad] = {}
+  new_hash[:railroads][:names][:pennsylvania_railroad] = {}
+  new_hash[:railroads][:names][:b_and_o_railroad] = {}
+  new_hash[:railroads][:names][:shortline_railroad] = {}
   new_hash
 end
 
