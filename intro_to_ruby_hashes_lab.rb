4c72634adaf1e17ae7057712a7fa7411f1c10c58
Christina Chianelli <cechianelli@gmail.com>
2020-05-23 19:58:55 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -13,7 +13,6 @@ def monopoly_with_second_tier
   # they'll guide you in how to modify what you just did to have more
   # complexity.
   new_hash[:railroads] = {:pieces => 4}
-  new_hash[:railroads] = {:rent_in_dollars => {}}
   new_hash
 end
 
