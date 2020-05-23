27721b6be7a641d5abe8809c325b26c94594551b
Christina Chianelli <cechianelli@gmail.com>
2020-05-23 20:14:54 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -34,7 +34,7 @@ def monopoly_with_third_tier
   new_hash[:railroads][:names][:reading_railroad] = {}
   new_hash[:railroads[:pennsylvania_railroad] = {}
   new_hash[:railroads][:b_and_o_railroad] = {}
-  new_hash[:shortline_railroad] = {}
+  new_hash[:railroads][:shortline_railroad] = {}
   new_hash
 end
 
