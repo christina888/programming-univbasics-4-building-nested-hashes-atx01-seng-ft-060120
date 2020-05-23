3bb34acc914eae1dfbdaa3baf42a283a79de9ac7
Christina Chianelli <cechianelli@gmail.com>
2020-05-23 19:55:36 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,7 +15,7 @@ def monopoly_with_second_tier
   new_hash = {}
   new_hash[:railroads] = {:pieces => 4}
   new_hash[:railroads][:rent_in_dollars] = {}
-  new_hash
+  new_hash[:railroads][:names] = {}
   
 end
 
