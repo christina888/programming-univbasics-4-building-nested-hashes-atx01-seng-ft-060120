33d4e09f19b5e3ce13f64aa40dedbf2341251723
Christina Chianelli <cechianelli@gmail.com>
2020-05-23 20:41:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,8 +26,12 @@ def monopoly_with_third_tier
   # complexity.
   new_hash = {}
   new_hash[:railroads] = {:pieces => 4}
-  new_hash[:railroads][:rent_in_dollars] = {:one_piece_owned => 25}
-  new_hash[:railroads][:rent_in_dollars][]
+  new_hash[:railroads][:rent_in_dollars] = {
+      :one_piece_owned=>25,
+      :two_pieces_owned=>50,
+      :three_pieces_owned=>100,
+      :four_pieces_owned=>200
+    }
   new_hash
 end
 
