dfeb32a98d98daa3921ec0c36e831df978e75dc1
Christina Chianelli <cechianelli@gmail.com>
2020-05-23 20:25:26 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -27,6 +27,7 @@ def monopoly_with_third_tier
   new_hash = {}
   new_hash[:railroads] = {:pieces => 4}
   new_hash[:railroads][:rent_in_dollars] = {:one_piece_owned => 25}
+  new_hash[:railroads][:rent_in_dollars] = {:one_piece_owned => 50}
   new_hash
 end
 
